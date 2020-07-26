de791b5fbe1b5fb6c8f35b4ab15569f4d7d324e0
Jose Cabrera <jcabrera@habitissimo.com>
2020-07-26 19:50:26 +0200

    update buttonn example

--- demo/storybook/Button/Button.tsx
+++ demo/storybook/Button/Button.tsx
@@ -1,3 +1,6 @@
 import React from 'react';
+import './button.scss';
 
-const Button: React.FC = () => <button>I'm a button</button>;
+const Button: React.FC = () => <button className="button">I'm a button</button>;
+
+export default Button;
