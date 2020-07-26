5137747cac14c1bfcfd3376538e4cd86ca20c361
Jose Cabrera <jcabrera@habitissimo.com>
2020-07-26 16:52:54 +0200

    convert stories to mdx format

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,8 @@
 const path = require('path');
 
 module.exports = {
-  stories: ['../src/**/*.stories.tsx'],
-  // Add any Storybook addons you want here: https://storybook.js.org/addons/
-  addons: [],
+  stories: ['../src/**/*.stories.mdx'],
+  addons: ['@storybook/addon-docs', '@storybook/preset-create-react-app'],
   webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
