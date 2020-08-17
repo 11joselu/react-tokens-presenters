a3d12ba31f71f404734f10d16990f69d5717e9e2
Jose Cabrera <jcabrera@habitissimo.com>
2020-08-17 19:19:45 +0200

    introduce tokentext and use in border component

--- src/presenters/Border/Border.tsx
+++ src/presenters/Border/Border.tsx
@@ -1,20 +1,17 @@
 import React, { FC } from 'react';
 import Card from '../../ui/Card/Card';
 import Box from '../../ui/Box/Box';
-import Text from '../../ui/Text/Text';
+import TokenText from '../../ui/Text/TokenText';
 
 import { Token } from '../TokenDeclaration';
 
 type BorderProps = { token: Token };
 
 const Border: FC<BorderProps> = ({ token }: BorderProps) => {
-  const hasRefence = typeof token.reference !== 'undefined';
   return (
     <Card data-testid="border">
       <Box border={token.value} backgroundColor="transparent"></Box>
-      <Text isAlternative={true}>
-        {hasRefence ? token.reference : token.value}
-      </Text>
+      <TokenText token={token} />
     </Card>
   );
 };
---
+++ src/ui/Text/TokenText.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from 'react';
+import { Token } from '../../presenters/TokenDeclaration';
+import Text from './Text';
+
+type TokenTextProps = {
+  token: Token;
+};
+
+const TokenText: FC<TokenTextProps> = ({ token }: TokenTextProps) => {
+  const hasRefence = typeof token.reference !== 'undefined';
+
+  return (
+    <Text isAlternative={true}>
+      {hasRefence ? token.reference : token.value}
+    </Text>
+  );
+};
+
+export default TokenText;
